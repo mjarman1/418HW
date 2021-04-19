015fe3672d9a846c1a950058d8aabe54031e3777
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-19 01:02:23 -0400

    hwpush

---
+++ app.js
@@ -0,0 +1,33 @@
+var m = 7;
+var j = 5;
+
+function addTwoVars(m,j){
+    console.log(m+j);
+}
+function subTwoVars(m,j){
+    console.log(m-j);
+}
+function timesTwoVars(m,j){
+    console.log(m*j);
+}
+function divideTwoVars(m,j){
+    console.log(m/j);
+}
+
+addTheVars = (m, j) => m + j;
+    console.log(addTheVars());
+
+subTheVars = (m, j) => m - j;
+     console.log(subTheVars());
+
+timesTheVars = (m, j) => m * j;
+     console.log(timesTheVars());
+
+divideTheVars = (m, j) => m / j;
+     console.log(divideTheVars());
+
+
+
+// multiplies the first input value by the second and returns it
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2); // returns 8
