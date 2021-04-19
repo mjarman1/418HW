d0a6b46ba3e1c8f90679d140bdbc4a8d5c3d5915
Marquis <Marquis.jayshaun.jarman@gmail.com>
2021-04-19 01:26:17 -0400

    fixHW

--- app.js
+++ app.js
@@ -2,32 +2,40 @@ var m = 7;
 var j = 5;
 
 function addTwoVars(m,j){
-    console.log(m+j);
+    return m+j;
+    
 }
+console.log(addTwoVars(m,j));
+
 function subTwoVars(m,j){
-    console.log(m-j);
+    return m-j;
 }
+console.log(subTwoVars(m,j));
+
 function timesTwoVars(m,j){
-    console.log(m*j);
+    return m*j;
 }
+console.log(timesTwoVars(m,j));
+
 function divideTwoVars(m,j){
-    console.log(m/j);
+    return m/j;
 }
+console.log(divideTwoVars(m,j));
 
 addTheVars = (m, j) => m + j;
-    console.log(addTheVars());
+    console.log(addTheVars(m,j));
 
 subTheVars = (m, j) => m - j;
-     console.log(subTheVars());
+     console.log(subTheVars(m,j));
 
 timesTheVars = (m, j) => m * j;
-     console.log(timesTheVars());
+     console.log(timesTheVars(m,j));
 
 divideTheVars = (m, j) => m / j;
-     console.log(divideTheVars());
+     console.log(divideTheVars(m,j));
 
 
 
 // multiplies the first input value by the second and returns it
-const multiplier = (item, multi) => item * multi;
-multiplier(4, 2); // returns 8
+// const multiplier = (item, multi) => item * multi;
+// multiplier(4, 2); // returns 8
